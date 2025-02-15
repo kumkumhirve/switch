e6e380e1e6d4ff604ccb7c2a2f69e3355d8afb3d
kumkumhirve <kumkumhirve452@gmail.com>
2025-02-15 18:40:41 +0530

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int day;
+   cout << "enter the day ";
+   cin >> day;
+   switch (day)
+   {
+   case 1:
+      cout << "monday";
+      break;
+   case 2:
+      cout << "tuesday";
+      break;
+   case 3:
+      cout << "wednesday";
+      break;
+   case 4:
+      cout << "thusday";
+      break;
+   case 5:
+      cout << "friday";
+      break;
+   case 6:
+      cout << "saturday";
+      break;
+   case 7:
+      cout << "sunday";
+      break;
+   default:
+      cout << "not a valied day";
+   }
+   return 0;
+}
